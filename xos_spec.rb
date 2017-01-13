4e80db2d1fe2db15c0fcd2dea8d130ab47c85bf8
Jon Black <jblack@JolleCocoon.local>
2017-01-13 13:26:02 -0500

    added to xos.spec.rb file

--- xos_spec.rb
+++ xos_spec.rb
@@ -3,5 +3,18 @@ require_relative 'XOs'
 
 
 describe XOs do
-  it 'can fill up the board'
+  skip 'can fill up the board' do
+
+    # c = Counter.new
+    # c.up
+    # expect(c.count).to eq 1
+    
+    x = XOs.new
+
+    # I fill up the board
+    9.times do
+      x.take_turn
+    end
+
+  end
 end
